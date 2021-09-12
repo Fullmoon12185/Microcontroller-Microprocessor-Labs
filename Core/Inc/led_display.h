2af7edf7c58db1adf64b4efba5066766a7c3d763
Nguyen Nguyen <nguyennth@aipower.com.vn>
2021-09-12 16:20:33 +0700

    adding proteus file to the repo

--- Core/Inc/led_display.h
+++ Core/Inc/led_display.h
@@ -8,7 +8,7 @@
 #ifndef INC_LED_DISPLAY_H_
 #define INC_LED_DISPLAY_H_
 
-void update_seven_segment_led_buffer(uint8_t val, uint8_t idx);
+uint8_t update_seven_segment_led_buffer(uint8_t val, uint8_t idx);
 void seven_segment_led_driver(void);
 
 #endif /* INC_LED_DISPLAY_H_ */
